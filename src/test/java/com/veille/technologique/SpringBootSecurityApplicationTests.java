c7793be6d2992dcb50783b74ca617c54dd4e1184
Richard Truong <richard.truong084@gmail.com>
2020-09-16 22:31:09 -0400

    Initial commit spring boot app

---
+++ src/test/java/com/veille/technologique/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veille.technologique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
