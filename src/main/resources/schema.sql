7ee357a8b8aa1afab9ce096d73a42db4222e427c
Richard Truong <richard.truong084@gmail.com>
2020-09-17 00:45:17 -0400

    POM + Authentication manager + HttpSecurity + JDBC/H2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+create table users(
+    username varchar_ignorecase(50) not null primary key,
+    password varchar_ignorecase(50) not null,
+    enabled boolean not null
+);
+
+create table authorities (
+    username varchar_ignorecase(50) not null,
+    authority varchar_ignorecase(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username, authority);
+
+
+INSERT INTO users (username, password, enabled) values ( 'user', 'pass', true );
+INSERT INTO users (username, password, enabled) values ( 'admin', 'pass', true );
+
+INSERT INTO authorities (username, authority) values ( 'user', 'ROLE_USER' );
+INSERT INTO authorities (username, authority) values ( 'admin', 'ROLE_ADMIN' );
